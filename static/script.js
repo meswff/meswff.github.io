9451591f1f03555fad34a4d43d0135b9ce97de39
meswff <147475534+meswff@users.noreply.github.com>
2024-01-25 18:14:38 +0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function show() {
+    element = document.querySelector('.other_div')
+    element.style.visibility = 'visible';;
+  }
+
+function hide() {
+element = document.querySelector('.other_div')
+element.style.visibility = 'visible';;
+}
