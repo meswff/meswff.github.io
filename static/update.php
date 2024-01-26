39a4aa98011a2b9461f57ed6485e7f7769b06362
meswff <147475534+meswff@users.noreply.github.com>
2024-01-26 19:55:42 +0300

    Update update.php

--- static/update.php
+++ static/update.php
@@ -1,7 +1,12 @@
 <?php
 
 	require_once 'usage.php'; //настройте данный конфигурационный файл
-	
+
+    header('Content-Type: application/json');
+    $aResult = array();
+    echo json_encode($aResult);
+
+
 	$res = $api->updateSales(array( 
         'id' => $argv[1],
         'sales_status_id' => $argv[2]
