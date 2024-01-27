f8ed26f391c8f45dc58e6094c50738bfdf961493
meswff <147475534+meswff@users.noreply.github.com>
2024-01-27 22:26:59 +0300

    Update update.php

--- static/update.php
+++ static/update.php
@@ -3,26 +3,23 @@
 	require_once 'usage.php'; //настройте данный конфигурационный файл
 
 
-	/*$res = $api->updateSales(array( 
-        'id' => $argv[1],
-        'sales_status_id' => $argv[2]
-    ));*/
-$arg = json_decode($argv[1])
+$arg = json_decode($argv[1]);
 
 $insert_status = $api->updateSales(array( 
 	array(    
-	'id' => $argv[1],
-	'sales_status_id' => $argv[2]
+	'id' => $arg['0']['0'],
+	'sales_status_id' => $arg['0']['1']
 	),  
 ));
 
 $insert = $api->insertEvent(array( 
-	'dtend' => $argv[3],
-	'description' => $argv[4],
+	'dtend' => $arg['0']['2'],
+	'description' => $arg['0']['3'],
 	'summary' => 'Автоматически созданное напоминание через телеграмм бота',
-	'connections' => $argv[1]
+	'connections' => $argv['0']['0']
 ));
 
 print_r($insert_status);
+print_r($insert);
 
 ?>
