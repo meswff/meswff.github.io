7348402f14697cdcabd9d32b2ddc35f2359334f9
meswff <147475534+meswff@users.noreply.github.com>
2024-01-27 22:02:21 +0300

    Update update.php

--- static/update.php
+++ static/update.php
@@ -8,18 +8,20 @@
         'sales_status_id' => $argv[2]
     ));*/
 
-    $insert_status = $api->updateSales(array( 
-    	array(    
+$insert_status = $api->updateSales(array( 
+	array(    
 	'id' => $argv[1],
 	'sales_status_id' => $argv[2]
-      ),  
-    ));
+	),  
+));
 
-    $insert = $api->insertEvent(array( 
-        'dtend' => $argv[3],
-        'description' => $argv[4],
-        'summary' => 'Автоматически созданное напоминание через телеграмм бота',
-        'connections' => $argv[1]
-    ));
+$insert = $api->insertEvent(array( 
+	'dtend' => $argv[3],
+	'description' => $argv[4],
+	'summary' => 'Автоматически созданное напоминание через телеграмм бота',
+	'connections' => $argv[1]
+));
+
+print_r($insert_status);
 
 ?>
