513de253b13a3da554d78db52601be9031eb9d69
meswff <147475534+meswff@users.noreply.github.com>
2024-01-27 18:19:23 +0300

    Delete update.php

--- update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require_once 'usage.php'; //настройте данный конфигурационный файл
-	
-	$res = $api->updateSales(array( 
-        'id' => $argv[1],
-        'sales_status_id' => $argv[2]
-    ));
-?>
