9666c83432815af728421b5f8419b56c9103b96d
meswff <147475534+meswff@users.noreply.github.com>
2024-01-27 23:53:23 +0300

    Add files via upload

---
+++ update.php
@@ -0,0 +1,38 @@
+<?php
+
+    require_once 'usage.php'; //настройте данный конфигурационный файл
+
+$insert_status = $api->updateSales(array( 
+	array(    
+        'id' => $argv[1],
+        'sales_status_id' => $argv[2]
+	),  
+));
+
+$result = $api->filterSales(array( 
+    'byid' => $argv[1],
+));
+
+
+$cust_id = $result['data']['list']['0']['employee_id'];
+
+$insert = $api->insertEvent(array( 
+    'event' => array(
+        'dtstart' => $argv[3],
+        'dtend' => $argv[3],
+        'description' => $argv[4],
+        'summary' => 'Автоматически созданное напоминание через телеграмм бота',
+        'connections' => $argv[1],
+        'author_id' => $cust_id,
+        'alarms' => array(
+            'trigger' => '-P10M',
+            'notice' => 'Автоматически созданное напоминание через телеграмм бота'
+        ),
+        'type_id' => '2'
+    ),
+));
+
+print_r($insert);
+print_r($insert_status);
+
+?>
