25871a4e3430b76b72d69159650e8291642a3b9b
meswff <147475534+meswff@users.noreply.github.com>
2024-01-27 18:18:18 +0300

    Update update.php

--- static/update.php
+++ static/update.php
@@ -2,13 +2,22 @@
 
 	require_once 'usage.php'; //настройте данный конфигурационный файл
 
-    header('Content-Type: application/json');
-    $aResult = array();
-    echo json_encode($aResult);
 
+	/*$res = $api->updateSales(array( 
+        'id' => $argv[1],
+        'sales_status_id' => $argv[2]
+    ));*/
 
-	$res = $api->updateSales(array( 
+    $insert_status = $api->updateSales(array( 
         'id' => $argv[1],
         'sales_status_id' => $argv[2]
     ));
+
+    $insert = $api->insertEvent(array( 
+        'dtend' => $argv[3],
+        'description' => $argv[4],
+        'summary' => 'Автоматически созданное напоминание через телеграмм бота',
+        'connections' => $argv[1]
+    ));
+;
 ?>
