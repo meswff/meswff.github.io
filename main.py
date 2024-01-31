35ae8f4e9d8019a596b2d6a353677377b33d11f9
meswff <147475534+meswff@users.noreply.github.com>
2024-01-31 21:49:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,14 @@ def index(id_user, id_offer):
     '73': 'АВТООБЗВОН',
     '72': 'Дубль/брак'
   }
-  if php('workers.php', str(id_user)) == id_user:
+  
+  userid = php('workers.php', str(id_user))
+  try:
+    my_dict = json.loads(userid)
+  except:
+    return render_template('403.html'), 403
+  
+  if my_dict == id_user:
     if statuses[sale_stage_id] == 'NEW' or statuses[sale_stage_id] == 'Звонок совершен' or statuses[sale_stage_id] == 'Собеседование назначено' or statuses[sale_stage_id] == 'Собеседование проведено' or statuses[sale_stage_id] == 'Обучение началось' or statuses[sale_stage_id] == 'Остался через 14 дней' or statuses[sale_stage_id] == 'Остался через 30 дней' or statuses[sale_stage_id] == 'Приняли на работу' or statuses[sale_stage_id] == 'Закрыли сделку':
       return render_template('index_job.html', user_username=name_full, user_id=customer_id, user_info=statuses[sale_stage_id], user_ip='В обработке', user_phone=phone, user_mail=email, user_status=statuses[sale_stage_id], lead_status=statuses[sale_stage_id], sale_id=id_offer, user_comment=my_dict['comment'])
     else:
