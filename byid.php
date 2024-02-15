e53b056e9b308774efafcd4fee50fc1f5758df4d
meswff <147475534+meswff@users.noreply.github.com>
2024-02-15 19:06:29 +0300

    Update byid.php

--- byid.php
+++ byid.php
@@ -20,11 +20,17 @@
 	$res_customer = $api->filterCustomers(array( 
 		'byid' => $id_customer
 	));
+
+	$res_comment = $api->getSalesComments(array( 
+		'byid' => $argv[1]
+	));
+	
 	
 	if (!is_array($res_customer)) {
-		$json_byid = json_encode($data['0']);
+		$json_byid = json_encode(array_merge($data['0'], $res_comment['data']['88662']['0']));
 	} else {
-		$json_byid = json_encode(array_merge($data['0'], $res_customer['data']['list']));
+		$merge = array_merge($data['0'], $res_customer['data']['list']);
+		$json_byid = json_encode(array_merge($merge, $res_comment['data']['88662']['0']));
 	}
 
 	print_r($json_byid);
