5dcadf08c62e6ddb683ef573c3d6908a5954578e
meswff <147475534+meswff@users.noreply.github.com>
2024-03-03 15:58:41 +0300

    Update getinfo.py

--- getinfo.py
+++ getinfo.py
@@ -16,32 +16,44 @@ def get_info_about_sale(sale_id):
     my_dict = json.loads(response.text)
     id_customer = my_dict['data']['list'][0]
 
-    try:
-        return {
-            'current_id': sale_id,
-            'employee_id': id_customer['employee_id'],
-            'id_offer': sale_id,
-            'sale_stage_id': id_customer['sale_stage_id']
-        }
-    except:
-        return 'Ошибка при попытке получить данные'
-
-
-def get_info_about_worker(employee_id):
-
-    url = 'http://aires.astoria-tula.ru:81/sharedapi/worker/filter'
+    url = 'http://aires.astoria-tula.ru:81/sharedapi/purchaser/filter'
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
     data = {
         'apikey': '21d1c8300ca07c06bf8f3aac3c16c275',
-        'params[id]': employee_id
+        'params[byid]': id_customer['customers_id']
     }
 
     response = requests.post(url, headers=headers, data=data)
-    my_dict = json.loads(response.text)
-    try:
-        return my_dict['data'][str(employee_id)]['fields']['3643']['value']
-    except:
-        return 'Информафия о работнике не найдена'
+    dict = json.loads(response.text)['data']['list'][0]
 
+    try:
+        name = str(dict['name']) + ' ' + str(dict['surname'])
+    except:
+        name = 'Отсутствует'
+
+    try:
+        email = dict['email'][0]['email']
+    except:
+        email = 'Отсутствует'
+
+    try:
+        if dict['text'] != None:
+            text = dict['text']
+    except:
+        text = 'Отсутствует'
+
+    try:
+        return {
+            'employee_id': id_customer['employee_id'],
+            'id_offer': sale_id,
+            'sale_stage_id': id_customer['sale_stage_id'],
+            'customer_id': id_customer['customers_id'],
+            'name': name,
+            'phone': dict['phone'][0]['phone'],
+            'text': text,
+            'email': email
+        }
+    except:
+        return 'Ошибка при попытке получить данные'
