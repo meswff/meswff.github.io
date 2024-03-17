5ca940959945de5b00770f4e7f7f64dc28a56432
meswff <147475534+meswff@users.noreply.github.com>
2024-03-17 18:09:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def index(id_crm, id_user, id_offer):
 def process_data():
   data = request.get_json()
 
-  return data
+  return render_template('101.html', error=data)
 
   saleid = data['saleid']
   year_and_moth = data['date'].split('-')
