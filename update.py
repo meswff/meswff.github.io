f86e577b5d62e9fa29ea1ea1811a884077a806aa
meswff <147475534+meswff@users.noreply.github.com>
2024-03-18 22:14:59 +0300

    Create update.py

---
+++ update.py
@@ -0,0 +1,30 @@
+import requests
+
+def change_stage(sale_id, stage):
+    url = 'http://aires.astoria-tula.ru:81/sharedapi/sales/update'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'apikey': '21d1c8300ca07c06bf8f3aac3c16c275',
+        'params[id]': sale_id,
+        'params[sales_status_id]': stage
+    }
+    
+    response = requests.post(url, headers=headers, data=data)
+    return True
+
+def add_comment(sale_id, comment, employee_id):
+    url = 'http://aires.astoria-tula.ru:81/sharedapi/sales/addComment'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'apikey': '21d1c8300ca07c06bf8f3aac3c16c275',
+        'params[entity_id]': sale_id,
+        'params[text]': comment,
+        'params[author]': employee_id
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    return True
