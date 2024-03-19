c25d970bee69632faed326bd1d057719fcc6cffa
meswff <147475534+meswff@users.noreply.github.com>
2024-03-19 13:28:27 +0300

    Update update.php

--- update.php
+++ update.php
@@ -3,34 +3,6 @@
     require_once 'usage.php'; //настройте данный конфигурационный файл
 
 
-$result = $api->filterSales(array( 
-    'byid' => $argv[1],
-));
-
-$cust_id = $result['data']['list']['0']['employee_id'];
-
-$insert = $api->insertEvent(array( 
-    'event' => array(
-        'dtstart' => $argv[3],
-        'dtend' => $argv[3],
-        'description' => $argv[4],
-        'summary' => 'Автоматически созданное напоминание через телеграмм бота',
-        'author_id' => $cust_id,
-        'alarms' => array(
-            'trigger' => '-P10M',
-            'notice' => 'Автоматически созданное напоминание через телеграмм бота'
-        ),
-        'type_id' => '2',
-        'connections' => array(
-            '0' => array(
-                'substance_summary' => 'Обращения',
-                'object_type' => 'crm_sale',
-                'object_id' => $argv[1]
-                ),
-            ), 
-    ),
-));
-
 $insert_status = $api->updateSales(array( 
 	array(    
         'id' => $argv[1],
@@ -38,16 +10,5 @@ $insert_status = $api->updateSales(array(
 	),  
 ));
 
-$evnt_id = (int)$argv[1] - 54471;
-
-$insert_status = $api->getEventsFilter(array( 
-    'event_id' => '33366',
-	),  
-);
-
-
-print_r($insert);
 print_r($insert_status);
-
-
 ?>
