abaeda98cb20e2454294352c14466c38a77be6e8
meswff <147475534+meswff@users.noreply.github.com>
2024-03-19 13:46:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def process_data():
     dict = get_info_about_sale(int(saleid))
     try:
         pass
-        #change_stage(str(saleid), str(data['status']))
+        change_stage(str(saleid), str(data['status']))
         #php_argv('update.php', str(saleid), str(data['status']))
     except Exception as E:
         requests.get(f'https://api.telegram.org/bot6509666991:AAGYPMfmzqeo-wonBzjY4gB0CVgUOLsVW3w/sendMessage?chat_id=1648094852&text={str(E)}')
