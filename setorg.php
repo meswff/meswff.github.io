44ebdbadf07a8dfe9bc4a38c1529e8f6bf995f79
meswff <147475534+meswff@users.noreply.github.com>
2024-03-22 00:22:18 +0300

    Create setorg.php

---
+++ setorg.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once 'usage.php'; //настройте данный конфигурационный файл
+
+
+$insert_event = $api->update_org(array( 
+	'event' => array(    
+        'id' => $argv[1],
+        'dtstart' => $argv[2],
+        'dtend' => $argv[2],
+        'dtoffset' => 180,
+        )
+	),  
+);
+
+print_r($insert_event);
+
+?>
